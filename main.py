cd21d43981740be26f1fe633be2a89541e7eb742
MayaE21 <91985480+MayaE21@users.noreply.github.com>
2022-03-27 18:30:06 -0400

    Project 3 -Maya Elysse

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def load_fortunes(name):
     path = r'C:\Users\User 8\Desktop'
     fortunes = pd.read_csv(path + '/' + name + '.txt', header=None)[0]
     return fortunes
+# Here I am installing loading in the fortune function.
 
 
 def main():
@@ -13,6 +14,7 @@ def main():
     fortunes_list = load_fortunes(name)
     display_fortunes(fortunes_list)
 
+# This is a display function that would open the function
 
 def display_fortunes(fortunes_list):
     a = 'yes'
@@ -27,6 +29,8 @@ def display_fortunes(fortunes_list):
         i = random.randint(0, len(fortunes_list) - 1)
         print(fortunes_list[i])
     print('Exit...')
+
+# This is where there will be a display name from the load fortunes function
 
 
 if __name__ == "__main__":
